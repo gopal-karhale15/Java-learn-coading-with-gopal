2a3b51bd463021abd970598e1abf9b782a8b6fb3
gopal12-coder <b3848946@gmail.com>
2025-12-02 10:46:06 +0530

    Second program to sum of three number

---
+++ src/addThreeNumber.java
@@ -0,0 +1,12 @@
+public class addThreeNumber {
+    public static void main(String[] args) {
+        System.out.println("the sum of three num");
+
+        int num1=50;
+        int num2=100;
+        int num3=50;
+        int sum=num1+num2+num3;
+
+        System.out.println(+sum);
+    }
+}
