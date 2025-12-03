d0ad6991b44217b72c18673f14f6aaea8cf66967
gopal12-coder <b3848946@gmail.com>
2025-12-03 14:42:55 +0530

    ✅ Professional Commit Messages 1. "feat: add triangle shape pattern program" 2. "refactor: clean code and improve formatting" 3. "feat: add user input support for row count" 4. "fix: correct alignment of printed triangle" 5. "docs: update README with usage instructions" 6. "chore: add .gitignore to ignore class files"

---
+++ src/triangleShape.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class triangleShape {
+
+    public static void main(String[] args) {
+
+        System.out.println("enter number to print triangle :");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++)
+                System.out.print(" * ");
+            System.out.println();
+        }
+
+
+    }
+
+}
